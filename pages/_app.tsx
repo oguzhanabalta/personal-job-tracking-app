cacbe3dadf5f5858de3018e87cff89fb1d709fad
oguzhan <oguzhanbalta99@gmail.com>
2022-04-26 00:33:25 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
+import {ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
+import {CacheProvider} from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-export default function MyApp(props) {
-    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+export default function MyApp({props}: any) {
+    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
 
     return (
         <CacheProvider value={emotionCache}>
             <Head>
                 <meta name="theme-color" content={theme.palette.primary.main}/>
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
                 <link
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
@@ -25,8 +25,9 @@ export default function MyApp(props) {
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
 
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-                      rel="stylesheet"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                    rel="stylesheet"/>
 
                 <link
                     rel="stylesheet"
@@ -35,7 +36,7 @@ export default function MyApp(props) {
             </Head>
             <ThemeProvider theme={theme}>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
+                <CssBaseline/>
                 <Component {...pageProps} />
             </ThemeProvider>
         </CacheProvider>
