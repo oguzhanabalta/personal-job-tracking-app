d4169166c5ec47adf1a8029f24f9c0e4b9879620
oguzhanbalta <oguzhanbalta99@gmail.com>
2022-04-25 17:03:20 +0300

    prettier settings, ssr, appbar.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,49 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import {CssBaseline, ThemeProvider} from "@mui/material";
-import theme from "../src/theme";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-  )
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
+export default function MyApp(props) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="theme-color" content={theme.palette.primary.main}/>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                      rel="stylesheet"/>
+
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
 
-export default MyApp
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    emotionCache: PropTypes.object,
+    pageProps: PropTypes.object.isRequired,
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,42 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import {AppBar, Button, Stack, Toolbar} from "@mui/material";
+import styled from "styled-components";
 
 export default function Index() {
+
+    const StyledTypography = styled(Typography)`
+      flex: 1;
+      font-size: 1.5rem;
+      font-weight: 400;
+`;
+
+    const  StyledAppBar = styled(AppBar)`
+      background:white;
+      border: 1px solid #e0e0e0;
+      width:100%;
+      color: blueviolet;
+    `;
+
   return (
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Personal Job Tracking App
-          </Typography>
-        </Box>
-      </Container>
+      <Stack>
+
+          <StyledAppBar position="static" color="primary">
+              <Toolbar>
+                  <StyledTypography variant={"h2"}  color="inherit">
+                      LOGO
+                  </StyledTypography>
+              </Toolbar>
+          </StyledAppBar>
+          <Container maxWidth="sm">
+              <Box sx={{ my: 4 }}>
+                  <Typography variant="h4" component="h1" gutterBottom>
+                      Personal Job Tracking App
+                  </Typography>
+              </Box>
+          </Container>
+      </Stack>
+
   );
 }
