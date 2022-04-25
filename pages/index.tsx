fe11d78c87ad76b62865d370eede821ffe19b60f
oguzhanbalta <oguzhanbalta99@gmail.com>
2022-04-25 17:54:01 +0300

    appbar component, create new job component added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,65 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import {AppBar, Button, Stack, Toolbar} from "@mui/material";
+import {Button, Stack, TextField} from "@mui/material";
 import styled from "styled-components";
+import AppBarComponent from "../src/components/AppBar";
+import {AddBox} from "@mui/icons-material";
 
 export default function Index() {
 
-    const StyledTypography = styled(Typography)`
-      flex: 1;
-      font-size: 1.5rem;
-      font-weight: 400;
-`;
 
-    const  StyledAppBar = styled(AppBar)`
-      background:white;
-      border: 1px solid #e0e0e0;
-      width:100%;
-      color: blueviolet;
+    const Title = styled(Typography)`
+      flex: 1;
+      font-size: 1.4rem;
+      font-weight: 500;
+      @media (max-width: 650px) {
+        font-size: 0.8rem;
+        color: #0070f3;
+      }
     `;
 
-  return (
-      <Stack>
+    const CreateButton = styled(Button)`
+      width: 10%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.7rem;
+      height: auto;
+      margin-top: 0.4rem;
+      margin-left: 1rem;
+      @media (max-width: 650px) {
+        margin-left: 0;
+      }
+    `;
 
-          <StyledAppBar position="static" color="primary">
-              <Toolbar>
-                  <StyledTypography variant={"h2"}  color="inherit">
-                      LOGO
-                  </StyledTypography>
-              </Toolbar>
-          </StyledAppBar>
-          <Container maxWidth="sm">
-              <Box sx={{ my: 4 }}>
-                  <Typography variant="h4" component="h1" gutterBottom>
-                      Personal Job Tracking App
-                  </Typography>
-              </Box>
-          </Container>
-      </Stack>
+    return (
+        <Stack sx={{}}>
+            <AppBarComponent/>
 
-  );
+            <Box sx={{my: 2, p: 5, pb: 0, display: "flex", justifyContent: "flex-start"}}>
+                <Title>
+                    Create new job
+                </Title>
+            </Box>
+            <Box sx={{pl: 5, pr: 5}}>
+                <Stack sx={{width: "100%", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+                    <TextField
+                        id="standard-basic"
+                        margin="normal"
+                        sx={{width: "70%"}}
+                    />
+                    <TextField
+                        id="standard-basic"
+                        margin="normal"
+                        sx={{width: "20%", ml: 2}}/>
+                    <CreateButton variant={"contained"}>
+                        <AddBox sx={{mr: 2}}/> CREATE
+                    </CreateButton>
+                </Stack>
+            </Box>
+
+        </Stack>
+
+    );
 }
