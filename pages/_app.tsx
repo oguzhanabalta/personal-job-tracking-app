9173ee722a8a51015bfc68044c59bc481c830140
oguzhan <oguzhanbalta99@gmail.com>
2022-04-26 00:49:33 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 import {CacheProvider} from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import {AppProps} from "next/app";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-export default function MyApp({props}: any) {
+export default function MyApp(props: AppProps) {
+    // @ts-ignore
     const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
 
     return (
